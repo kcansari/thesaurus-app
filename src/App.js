25daa77613a008040ef2fdfafd84bb88fa3a32f6
kcansari <kcansari0@gmail.com>
2022-11-16 16:42:13 +0300

    rebuild to deployment

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import './App.css';
-import * as React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import theme from './Components/theme';
-import {BrowserRouter,Routes, Route} from "react-router-dom";
-import Home from './Pages/Home';
-import Definition from './Pages/Definition';
-import Grid from '@mui/material/Grid';
+import './App.css'
+import * as React from 'react'
+import { ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+import theme from './Components/theme'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Definition from './Pages/Definition'
+import Grid from '@mui/material/Grid'
 
-
-
-
+// latest verison
 function App() {
   return (
     <ThemeProvider theme={theme}>
-       <CssBaseline/>
-       <Grid container>
-       <Grid item xs={12} sx={{p:2}}>
-       <BrowserRouter>
+      <CssBaseline />
+      <Grid container>
+        <Grid item xs={12} sx={{ p: 2 }}>
+          <BrowserRouter>
             <Routes>
-              <Route path='/'  element=<Home /> />
+              <Route path='/' element=<Home /> />
               <Route path='/definition/:word' element=<Definition /> />
             </Routes>
           </BrowserRouter>
-          </Grid>
-       </Grid>
+        </Grid>
+      </Grid>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
